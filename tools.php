1d573da92a6aeffc6a79130448861fa9206cdc77
Jason Zhang[archlinux] <mock2u@gmail.com>
2013-07-26 00:08:45 +0800

    refactor quota, ls, cp, mv, rm, mkdir and add help

--- tools.php
+++ tools.php
@@ -10,17 +10,106 @@ require_once('libs/FunctionWrapper.php');
 // init
 $pcs = new BaiduPCS($access_token);
 
-// test case
+if (! isset($argv[1])) {
+  $argv[1] = 'help';
+}
 
-/*
- * funQuota()
- *
- */
-$return_funQuota = funQuota($pcs);
-$return_funQuota_quota = $return_funQuota->{'quota'} / 1024 / 1024 / 1024;
-$return_funQuota_used = $return_funQuota->{'used'} / 1024 / 1024 / 1024;
-echo 'Quota: ' . $return_funQuota_used . ' GB /' . $return_funQuota_quota . " GB \n";
-echo "\n";
+switch($argv[1]) {
+  case 'help':
+    if (isset($argv[2])) {
+      switch($argv[2]) {
+        case 'quota':
+          echo "``./tools.php $argv[2]``\n";
+          break;
+        case 'list':
+        case 'ls':
+          // TODO
+          echo "``./tools.php $argv[2] [_dir/file_]``\n";
+          break;
+        case 'move':
+        case 'mv':
+        case 'copy':
+        case 'cp':
+          echo "``./tools.php $argv[2] _src_ _dst_``\n";
+          break;
+        case 'remove':
+        case 'rm':
+          echo "``./tools.php $argv[2] _file_/_dir_ -- _dir_ didn't try\n";
+        case 'mkdir':
+          echo "``./tools.php $argv[2] _dir_\n";
+        default:
+          break;
+      }
+    }
+    else {
+      echo <<<EOF
+Basic commands:
+  quota
+  list / ls
+  copy / cp
+  move / mv
+  remove / rm
+  mkdir
+  upload
+  download
+
+Extended commands:
+  todo
+EOF;
+      echo "\n";
+    }
+    break;
+  case 'quota':
+    $return_funQuota = funQuota($pcs);
+    $return_funQuota_quota = $return_funQuota->{'quota'} / 1024 / 1024 / 1024;
+    $return_funQuota_used = $return_funQuota->{'used'} / 1024 / 1024 / 1024;
+    echo 'Quota: ' . $return_funQuota_used . ' GB /' . $return_funQuota_quota . " GB \n";
+    break;
+  case 'list':
+  case 'ls':
+    $return_funListFiles = funListFiles($pcs);
+    $return_funListFiles_length = sizeof($return_funListFiles["list"]);
+    for ($loop = 0; $loop < $return_funListFiles_length; ++$loop) {
+      echo $return_funListFiles["list"][$loop]["path"] . "\n";
+    }
+    break;
+  case 'copy':
+  case 'cp':
+    if (! isset($argv[2]) || ! isset($argv[3])) {
+      exit("Try ``./tools.php help $argv[1]`` for help\n");
+    }
+    $return_funCopySingle = funCopySingle($pcs, $argv[2], $argv[3]);
+    break;
+  case 'move':
+  case 'mv':
+    if (! isset($argv[2]) || ! isset($argv[3])) {
+      exit("Try ``./tool.php help $argv[1]`` for help\n");
+    }
+    $return_move_single = funMoveSingle($pcs, $argv[2], $argv[3]);
+    break;
+  case 'remove':
+  case 'rm':
+    if (! isset($argv[2])) {
+      exit("Try ``./tools.php help $argv[1]`` for help\n");
+    }
+    $return_delete_single = funDeleteSingle($pcs, $argv[2]);
+    break;
+  case 'mkdir':
+    if (! isset($argv[2])) {
+      exit("Try ``./tools.php help $argv[1]`` for help\n");
+    }
+    $return_make_dir = funMakeDir($pcs, $argv[2]);
+    break;
+  case 'upload':
+    break;
+  case 'download':
+    break;
+  default:
+    echo "Try ``./tools.php help`` for help\n";
+    break;
+}
+
+// test case
 
 /*
  * funDiff()
@@ -32,20 +121,6 @@ echo "\n";
 //echo "\n";
 
 /*
- * funListFiles()
- *
- */
-/*
-$return_funListFiles = funListFiles($pcs);
-$return_funListFiles_length = sizeof($return_funListFiles["list"]);
-//print_r($return_funListFiles);
-for ($loop = 0; $loop < $return_funListFiles_length; ++$loop) {
-  echo $return_funListFiles["list"][$loop]["path"] . "\n";
-}
-echo "\n";
- */
-
-/*
  * funUploadFile()
  *
  */
@@ -54,24 +129,11 @@ echo "\n";
 //print_r($return_funUploadFile);
 
 /*
- * funDeleteSingle()
- *
- */
-//$return_delete_single = funDeleteSingle($pcs, 'demo'); //print_r($return_delete_single);
-
-/*
  * funMakeDir()
  *
  */
 //$return_make_dir = funMakeDir($pcs, 'foo');
 //print_r($return_make_dir);
-
-/*
- * funMoveSingle()
- *
- */
-//$return_move_single = funMoveSingle($pcs, '23.jpg', '233.jpg');
-//print_r($return_move_single);
 
 /*
  * funCreateSuperFile()
@@ -99,6 +161,7 @@ echo "\n";
  * funGetMeta()
  *
  */
+/*
 $return_get_meta = funGetMeta($pcs, '1.pdf');
 $return_get_meta_list_ctime = $return_get_meta['list']['0']['ctime'];
 $return_get_meta_list_mtime = $return_get_meta['list']['0']['mtime'];
@@ -110,6 +173,7 @@ if ($return_get_meta['list']['0']['isdir']) {
   echo '/';
 }
 echo "\n";
+ */
 //print_r($return_get_meta);
 
 /*
@@ -135,16 +199,4 @@ if ($return_funCopySingle) {
 echo "\n";
  */
 
-/*
- * list again
- *
- */
-/*
-$return_funListFiles = funListFiles($pcs);
-$return_funListFiles_length = sizeof($return_funListFiles['list']);
-for ($loop = 0; $loop < $return_funListFiles_length; ++$loop) {
-  echo $return_funListFiles["list"][$loop]["path"] . "\n";
-}
-echo "\n";
- */
 ?>
